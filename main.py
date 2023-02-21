8a7075e116514c9c37c7506c5295097b1196830f
bonnie <owu.owu.666@gmail.com>
2023-02-21 11:49:20 -0800

    step one websters api
    i began utilizing the websters dictionaty api to put take words from our txt file and put them into our websters api

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from urllib.request import urlopen
-url = "https://webstersdictionary1828.com/Dictionary/Abaddon"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-print(html)
+import requests
+import json
+from pprint import pprint
+count = 0
+dictionary = open("words_dictionary.json.txt")
+for key in dictionary: 
+    word = key
+    # Call an API
+    url = "https://dictionaryapi.com/api/v3/references/collegiate/json/"+ word +"?key=207a976f-67d9-4375-b8fd-ce6c8ca3f5bc"
+    response = requests.get(url)
+    response.raise_for_status() # check for errors
+
+    # Load JSON data into a Python variable.
+    jsonData = json.loads(response.text)
+    pprint(jsonData)
+    
