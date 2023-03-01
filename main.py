6a19660cdbdf2dff21921710c533d940f0c52f50
bonnie <owu.owu.666@gmail.com>
2023-03-01 11:05:24 -0800

    json shit
    figuring out how turning the json file into a usable dictionary

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
 import json
 from pprint import pprint
-count = 0
 dictionary = open("words_dictionary.json.txt")
-for key in dictionary: 
-    word = key
-    # Call an API
-    url = "https://dictionaryapi.com/api/v3/references/collegiate/json/"+ word +"?key=207a976f-67d9-4375-b8fd-ce6c8ca3f5bc"
-    response = requests.get(url)
-    response.raise_for_status() # check for errors
-
-    # Load JSON data into a Python variable.
-    jsonData = json.loads(response.text)
-    pprint(jsonData)
+print(type(dictionary))
+# for key in dictionary: 
+#     # word = key
     
+    
+    
+#     print(key)
+'''
+# Call an API
+url = "https://dictionaryapi.com/api/v3/references/collegiate/json/"+ word +"?key=207a976f-67d9-4375-b8fd-ce6c8ca3f5bc"
+response = requests.get(url)
+response.raise_for_status() # check for errors
+# Load JSON data into a Python variable.
+jsonData = json.loads(response.text)
+pprint(jsonData)
+'''
