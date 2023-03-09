2dd6a6b2bcb9709266d61bab17ebd708f2c45bc8
bonnie <owu.owu.666@gmail.com>
2023-03-09 10:28:28 -0800

    organized the json file
    stripped the value from the key value pair

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import requests
 import json
 from pprint import pprint
-dictionary = open("words_dictionary.json.txt")
-print(type(dictionary))
-# for key in dictionary: 
-#     # word = key
-    
-    
-    
-#     print(key)
-'''
-# Call an API
-url = "https://dictionaryapi.com/api/v3/references/collegiate/json/"+ word +"?key=207a976f-67d9-4375-b8fd-ce6c8ca3f5bc"
-response = requests.get(url)
-response.raise_for_status() # check for errors
-# Load JSON data into a Python variable.
-jsonData = json.loads(response.text)
-pprint(jsonData)
-'''
+
+ 
+# Open the sample JSON file
+# Using the open() function
+file = open("words_dictionary.json.txt", 'r')
+ 
+# Convert the JSON data into Python object
+# Here it is a dictionary
+json_data = json.load(file)
+ 
+# Check the type of the Python object
+# Using type() function 
+print(type(json_data))
+ 
+# Iterate through the dictionary
+# And print the key: value pairs
+for key, value in json_data.items():
+    word = (f"\nKey: {key}")
+    print (word)
+ #insert ria code     
+ 
+# Close the opened sample JSON file
+# Using close() function
+file.close()
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(f"\nKey: {key}")
+    
