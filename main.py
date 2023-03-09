3c46a82f3194d70a3fcd9ee1c0417ea917fed878
Ria Alluri <85642177+riaalluri@users.noreply.github.com>
2023-03-09 11:30:54 -0800

    pytrends avg number

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+"""
 import requests
 import json
 from pprint import pprint
+<<<<<<< Updated upstream
 dictionary = open("words_dictionary.json.txt")
 print(type(dictionary))
 # for key in dictionary: 
@@ -18,3 +20,45 @@ response.raise_for_status() # check for errors
 jsonData = json.loads(response.text)
 pprint(jsonData)
 '''
+=======
+# Open the sample JSON file
+# Using the open() function
+file = open("words_dictionary.json.txt", 'r')
+ 
+# Convert the JSON data into Python object
+# Here it is a dictionary
+json_data = json.load(file)
+ 
+# Check the type of the Python object
+# Using type() function 
+print(type(json_data))
+ 
+# Iterate through the dictionary
+# And print the key: value pairs
+for key, value in json_data.items():
+    word = f"{key}"
+    print(word)
+ #  set = {
+#      value,
+#      value, 
+#      vaue,
+#  }
+# Close the opened sample JSON file
+# Using close() function
+file.close()
+"""
+
+import pandas as pd 
+from pytrends.request import TrendReq
+pytrends = TrendReq(hl='en-US', tz=360)
+
+"""
+
+words = ['melancholy']
+# payload
+pytrends.build_payload(words, cat=0, timeframe='today 12-m')
+
+"""
+pytrends.build_payload(kw_list=['pizza', 'bagel'], timeframe=['2022-09-04 2022-09-10', '2022-09-18 2022-09-24'])
+print(pytrends.multirange_interest_over_time())
+>>>>>>> Stashed changes
