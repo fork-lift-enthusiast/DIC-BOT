9aa71979494d6f96e910cacaa19d7a0838aaad2a
Ria Alluri <85642177+riaalluri@users.noreply.github.com>
2023-04-17 10:40:44 -0700

    pull trends score
    pulls trends score

--- main.py
+++ main.py
@@ -46,19 +46,70 @@ for key, value in json_data.items():
 # Close the opened sample JSON file
 # Using close() function
 file.close()
-"""
 
+import requests
+proxy_servers = {
+    'http': 'http://proxy.sample.com:8080',
+    'https': 'http://secureproxy.sample.com:8080'
+}
+s = requests.Session()
+s.proxies = proxy_servers
+response = s.get('https://trends.google.com/home?geo=US')
 import pandas as pd 
 from pytrends.request import TrendReq
 pytrends = TrendReq(hl='en-US', tz=360)
+print("test")
 
-"""
 
 words = ['melancholy']
 # payload
 pytrends.build_payload(words, cat=0, timeframe='today 12-m')
 
-"""
+
 pytrends.build_payload(kw_list=['pizza', 'bagel'], timeframe=['2022-09-04 2022-09-10', '2022-09-18 2022-09-24'])
 print(pytrends.multirange_interest_over_time())
->>>>>>> Stashed changes
+
+"""
+
+import json
+from pytrends.request import TrendReq
+
+# Set up the pytrends client with your authentication credentials
+pytrends = TrendReq()
+
+# Read the JSON file and parse it
+with open('words_dictionary.json.txt') as f:
+    try:
+        data = json.load(f)
+    except ValueError as e:
+        print("Error parsing JSON file:", str(e))
+        exit()
+
+# Extract all the words from the data
+words = []
+for item in data:
+    if not isinstance(item, dict):
+        print("Error: item is not a dictionary:", item)
+        continue
+    if 'words' not in item:
+        print("Error: 'words' key not found in item:", item)
+        continue
+    words.extend(item['words'])
+
+# Retrieve the trends data for each word
+trends_data = {}
+for word in words:
+    pytrends.build_payload([word], timeframe='today 5-y')
+    interest_over_time_df = pytrends.interest_over_time()
+    if interest_over_time_df.empty:
+        # No data available for the word
+        continue
+    # Calculate the average score for the word
+    average_score = interest_over_time_df[word].mean()
+    trends_data[word] = average_score
+
+# Create a dictionary with words that have an average score less than 10
+result = {word: score for word, score in trends_data.items() if score < 10}
+
+# Print the resulting dictionary
+print(result)
